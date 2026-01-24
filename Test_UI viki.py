e887aa0bc9dc8c4e42a81a31624e1a5ae391ef50
ArtemB2105 <165659129+ArtemB2105@users.noreply.github.com>
2026-01-24 20:43:15 +0300

    Delete Test_UI viki.py

--- Test_UI viki.py
+++
@@ -1,7 +0,0 @@
-from playwright.sync_api import Page, expect
-
-
-def test_wiki(page: Page):
-    page.goto("https://ru.wikipedia.org/")
-    page.get_by_role(role="link", name="Содержание").click()
-    expect(page.locator("#firstHeading")).to_have_text("Википедия:Содержание")
